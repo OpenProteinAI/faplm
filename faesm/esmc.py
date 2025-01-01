004c629adac5e53a82857d6f9865d98f29f01ee5
Timothy Fei Truong Jr <ttruong@ne47.bio>
2025-01-01 00:36:30 +0000

    fix sdpa attn bias device

--- faesm/esmc.py
+++ faesm/esmc.py
@@ -114,10 +114,10 @@ def scaled_dot_product_attention(query, key, value, attn_mask=None, dropout_p=0.
         is_causal=False, scale=None, enable_gqa=False) -> tuple[torch.Tensor, torch.Tensor]:
     L, S = query.size(-2), key.size(-2)
     scale_factor = 1 / math.sqrt(query.size(-1)) if scale is None else scale
-    attn_bias = torch.zeros(L, S, dtype=query.dtype)
+    attn_bias = torch.zeros(L, S, dtype=query.dtype, device=query.device)
     if is_causal:
         assert attn_mask is None
-        temp_mask = torch.ones(L, S, dtype=torch.bool).tril(diagonal=0)
+        temp_mask = torch.ones(L, S, dtype=torch.bool, device=query.device).tril(diagonal=0)
         attn_bias.masked_fill_(temp_mask.logical_not(), float("-inf"))
         attn_bias.to(query.dtype)
 
